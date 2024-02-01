4c43addee9eb71b139261bf3743d154dbb3a0b02
Ke Chang <ke.chang@1und1.de>
2024-02-01 16:55:10 +0100

    Post data as JSON

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 2.5.3
+
+2024-02-01
+
+### Added
+
+- RSS full-text
+
+### Changed
+
+- Post data as JSON
+
 ## 2.4.0
 
 2023-12-31
--- data/post-list.mjs
+++ data/post-list.mjs
@@ -1,71 +1,33 @@
 import {readFileSync} from 'node:fs';
 import * as path from 'node:path';
+import {createRequire} from 'node:module';
 
-const formatDate = dateStr => {
+const require = createRequire(import.meta.url);
+
+const genPubDateObj = dateStr => {
   const dt = new Date(dateStr);
-  return dt.toUTCString();
+  return {
+    dateTime: dt.toUTCString(),
+    label: dt.toDateString()
+  };
 };
 
-const loadFeedContent = (p) => {
-  const filePath = path.join('public', p, 'index.txt');
-  const content = readFileSync(filePath, {encoding: 'utf-8'});
-  const contentArray = content.split('\n');
-  contentArray.splice(0, 1);
-  return contentArray.join('\n');
-};
-
-const postList = [
-  {
-    title: 'Chrome Mobile Font Size Issue',
-    pubDate: {
-      dateTime: formatDate('2023-10-21'),
-      label: 'Oct 21 2023'
-    },
-    url: '/posts/chrome-mobile-font-size/'
-  },
-  {
-    title: 'Normal JavaScript Included As Module',
-    pubDate: {
-      dateTime: formatDate('2023-09-06'),
-      label: 'Sep 06 2023'
-    },
-    url: '/posts/normal-js-included-as-module/'
-  },
-  {
-    title: 'How to Lint Lit Styles',
-    pubDate: {
-      dateTime: formatDate('2023-06-05'),
-      label: 'Jun 05 2023'
-    },
-    url: '/posts/howto-lint-lit-styles/'
-  },
-  {
-    title: 'Introducing "Shoelace"',
-    pubDate: {
-      dateTime: formatDate('2021-10-29'),
-      label: 'Oct 29 2021'
-    },
-    url: '/posts/introducing-shoelace/'
-  },
-  {
-    title: 'Setting Up Lit',
-    pubDate: {
-      dateTime: formatDate('2021-08-28'),
-      label: 'Aug 28 2021'
-    },
-    url: '/posts/setting-up-lit/'
-  },
-  {
-    title: 'Hello World!',
-    pubDate: {
-      dateTime: formatDate('2021-07-22'),
-      label: 'Jul 22 2021'
-    },
-    url: '/posts/hello-world/'
+const loadFeedContent = (pathPart) => {
+  try {
+    const filePath = path.join('public', pathPart, 'index.txt');
+    const content = readFileSync(filePath, {encoding: 'utf-8'});
+    const contentArray = content.split('\n');
+    contentArray.splice(0, 1); // remove the headline (<h1>) since it's shown in reader as title already
+    return contentArray.join('\n');
+  } catch {
+    return 'Full-text available on website.';
   }
-];
+};
+
+const postList = require('./post-list.json');
 
 postList.forEach(post => {
+  post['pubDate'] = genPubDateObj(post.pubDate);
   post['content'] = () => loadFeedContent(post.url);
 });
 
